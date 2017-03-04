79cdb1612dd4a74583b33049eb752e3ce245eaf8
Sebastian Krysmanski <noreply@manski.net>
2017-03-04 09:22:11 +0100

    Both tests now print the used NodeJS verison (as this may be important for the test).

--- test-child-process.js
+++ test-child-process.js
@@ -1,5 +1,8 @@
 const child_process = require('child_process');
 
+console.log('==> Running NodeJS ' + process.version);
+console.log();
+
 child_process.spawnSync('docker', [ 'run', '--rm', '-ti', 'krys/ctrl-c-test' ], {
 	//cwd: options.cwd || process.cwd(),
 	stdio: 'inherit'
--- test-shelljs.js
+++ test-shelljs.js
@@ -1,5 +1,8 @@
 require('shelljs/global');
 
+console.log('==> Running NodeJS ' + process.version);
+console.log();
+
 exec('docker run --rm -ti krys/ctrl-c-test');
 
 console.log();
